c02d473a678f9cac7caa22158b52a1838bd953db
Nataliya Kashaeva <n.a.kashaeva@gmail.com>
2023-03-16 18:19:03 +0300

    Remove code-duplicating comments

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -16,6 +16,6 @@ def storage_information_view(request):
         non_closed_visits.append(visit_info)
 
     context = {
-        'non_closed_visits': non_closed_visits,  # не закрытые посещения
+        'non_closed_visits': non_closed_visits,
     }
     return render(request, 'storage_information.html', context)
